51d592cb894861f6bc1b96e6e05cf60abccbef61
Coman Nicu <ncoman32@yahoo.com>
2019-11-02 16:24:45 +0200

    Tabulated Version

---
+++ globaldata.c
@@ -0,0 +1,12 @@
+//
+// Created by nicu on 16.10.2019.
+//
+
+#include "globaldata.h"
+//  globaldata.c
+//  Simulation Methods course, 2018
+//  First Assignment: Molecular Dynamics (Brownian Dynamics) Simulation
+
+#include "globaldata.h"
+
+struct global_struct global;
